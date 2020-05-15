2c6f5272ffcc9a5efbeeb5d0b2e7a9c007524535
Claudimar Bezerra <claudimards@gmail.com>
2020-05-15 12:26:29 -0300

    definido quando a cobrinha aumenta de tamanho e local de aparecer a nova comida

--- js/script.js
+++ js/script.js
@@ -47,9 +47,9 @@ function update( event ){ // códigos da direções 37 -> esquerda | 38 -> cima 
 function startGame(){
     // permitindo que a cobrinha atravesse a tela de um lado para o outro
     if( snake[0].x > 15 * box && direction === "right" ) snake[0].x = 0;
-    if( snake[0].x < 0 && direction === "left") snake[0].x = 16 * box;
+    if( snake[0].x < 0 && direction === "left") snake[0].x = 15 * box;
     if( snake[0].y > 15 * box && direction === "down" ) snake[0].y = 0;
-    if( snake[0].y < 0 && direction === "up" ) snake[0].y = 16 * box;
+    if( snake[0].y < 0 && direction === "up" ) snake[0].y = 15 * box;
 
     // chamando a função que cria a tela do jogo na página
     createBG();
@@ -67,7 +67,14 @@ function startGame(){
     if( direction == "left" ) snakeX -= box;
     if( direction == "up" ) snakeY -= box;
     if( direction == "down" ) snakeY += box;
-    snake.pop();
+
+    // definindo quando a cobrinha aumentará de tamanho
+    if( snakeX !== food.x || snakeY !== food.y ){
+        snake.pop(); // cobrinha não aumenta de tamanho
+    }else{
+        food.x = Math.floor( Math.random() * 15 + 1 ) * box,
+        food.y = Math.floor( Math.random() * 15 + 1 ) * box
+    }
 
     // criando a cobrinha inicial
     let newHead = {
