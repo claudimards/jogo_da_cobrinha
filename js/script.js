7565b687ee57042567c2298508f7ddae26f1b232
Claudimar Bezerra <claudimards@gmail.com>
2020-05-15 00:48:01 -0300

    Movimentos da cobrinha definidos

--- js/script.js
+++ js/script.js
@@ -2,10 +2,11 @@ let canvas = document.getElementById("snake"); // recuperando id do canvas para 
 let context = canvas.getContext("2d"); // definindo canvas como elemento 2d
 let box = 32; // definindo a quantidade de quadros dentro do jogo
 let snake = []; // array para criar a cobrinha
-snake[0] = { // definindo posição inicial da cobrinha
+snake[0] = { // definindo tamanho da cobrinha
     x: 8 * box,
     y: 8 * box
 }
+let direction = "right"; // direção da cobrinha
 
 // função para criar o backgroundo do jogo
 function createBG(){
@@ -21,8 +22,32 @@ function createSnake(){
     }
 }
 
-// chamando a função que cria a tela do jogo na página
-createBG();
+// função para iniciar o jogo
+function startGame(){
+    // chamando a função que cria a tela do jogo na página
+    createBG();
+    // chamando a função que cria a cobrinha
+    createSnake();
 
-// chamando a função que cria a cobrinha
-createSnake();
+    // definindo o ponto incial da cobrinha
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    // definindo para onde a cobrinha irá se mover
+    if( direction == "right" ) snakeX += box;
+    if( direction == "left" ) snakeX -= box;
+    if( direction == "up" ) snakeY -= box;
+    if( direction == "down" ) snakeY += box;
+    snake.pop();
+
+    // criando a cobrinha inicial
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+    snake.unshift(newHead); // adicionando sempre um elemento no inicio da cobrinha
+}
+
+
+// definindo tempo de atualização de quadros do jogo
+let game = setInterval( startGame, 100);
