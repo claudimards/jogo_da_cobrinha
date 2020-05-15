f6286043f466f4cc4cccee317c27ce27a67fc1c1
Claudimar Bezerra <claudimards@gmail.com>
2020-05-15 10:44:56 -0300

    direções definidas e permitindo que a cobrinha passe dum canto da tela para o outro nos 4 cantos]

--- js/script.js
+++ js/script.js
@@ -22,8 +22,25 @@ function createSnake(){
     }
 }
 
+// criando eventos para captar o botão pressionado no teclado
+document.addEventListener('keydown', update);
+
+// função para atualizar a tecla pressionada
+function update( event ){ // códigos da direções 37 -> esquerda | 38 -> cima | 39 -> direita | 40 -> baixo
+    if( event.keyCode === 37 && direction !== "right" ) direction = "left";
+    if( event.keyCode === 38 && direction !== "down" ) direction = "up";
+    if( event.keyCode === 39 && direction !== "left" ) direction = "right";
+    if( event.keyCode === 40 && direction !== "up" ) direction = "down";
+}
+
 // função para iniciar o jogo
 function startGame(){
+    // permitindo que a cobrinha atravesse a tela de um lado para o outro
+    if( snake[0].x > 15 * box && direction === "right" ) snake[0].x = 0;
+    if( snake[0].x < 0 && direction === "left") snake[0].x = 16 * box;
+    if( snake[0].y > 15 * box && direction === "down" ) snake[0].y = 0;
+    if( snake[0].y < 0 && direction === "up" ) snake[0].y = 16 * box;
+
     // chamando a função que cria a tela do jogo na página
     createBG();
     // chamando a função que cria a cobrinha
@@ -47,7 +64,6 @@ function startGame(){
     }
     snake.unshift(newHead); // adicionando sempre um elemento no inicio da cobrinha
 }
-
 
 // definindo tempo de atualização de quadros do jogo
 let game = setInterval( startGame, 100);
